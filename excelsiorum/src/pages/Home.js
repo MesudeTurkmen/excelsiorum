89b91a6caa674ddf6ce44ac81a12b3e117d4d4e2
MesudeTurkmen <mesudeturkmen0@google.com>
2025-01-29 00:23:55 +0300

    v0.1

---
+++ excelsiorum/src/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+      <div>
+        <h1>Excelsiorum Yarışmasına Hoş Geldiniz!</h1>
+        <p>Burada önceki projeleri inceleyebilir, yarışmaya katılabilirsiniz.</p>
+      </div>
+    );
+  };
+  
+  export default Home;
+  
